2fdc48c51c4c31fef5d9d48ebcabb02a7f1308fb
Tar Gergő <tar.gergo21@gmail.com>
2025-02-25 15:22:00 +0100

    meg kapjuk az adatokat.

--- src/components/MenuTable.js
+++ src/components/MenuTable.js
@@ -1,20 +1,12 @@
-import React, { useContext, useState } from "react";
-import { Button, Modal, Table } from "react-bootstrap";
+import React, { useContext } from "react";
+import { Button, Table } from "react-bootstrap";
 import AdatokContext from "../contexts/AdatokContext";
 import MenuTableSor from "./MenuTableSor";
-import MenuUj from "./MenuUj";
-import AltalanosForm from "./AltalanosForm";
+
 
 function MenuTable() {
   const { menuLista } = useContext(AdatokContext);
-  const [showForm, setShowForm] = useState(false); // Állapot a form megjelenítéséhez
 
-  // Függvény, amely eltünteti a formot
-  const handleFormSubmit = () => {
-    setShowForm(false); // Form elrejtése
-  };
-  const handleShow = () => setShowForm(true); // A form megjelenítése
-  const handleClose = () => setShowForm(false); // A form bezárása
   return (
     <div>
       <div className="">
@@ -28,13 +20,7 @@ function MenuTable() {
           Mentés
         </Button>
       </div>
-      <AltalanosForm
-        apiEndpoint="/menus"
-        formTitle="Új Menü Felvétele"
-        onFormSubmit={() => {
-         
-        }}
-      />
+   
 
       <Table striped bordered hover>
         <thead className="text-center">
