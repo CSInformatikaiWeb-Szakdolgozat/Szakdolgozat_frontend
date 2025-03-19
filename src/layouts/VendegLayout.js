c9b7b08d020e6aa0b5aa285749c7f29a2e7a03d3
Zerghorda21 <tar.gergo21@gmail.com>
2025-03-19 14:57:03 +0100

    teszt navmeg csinálva (Alex)

--- src/layouts/VendegLayout.js
+++ src/layouts/VendegLayout.js
@@ -5,8 +5,9 @@ import { Navigate, Outlet } from "react-router-dom";
 import useAuthContext from "../contexts/AuthContext";
 import Navigacio from "../pages/public/Navigacio";
 import NavJobbOldal from "../pages/public/NavJobbOldal";
+import NavigacioTeszt from "../pages/public/dinamikus/NavigacioTeszt"
 
 export default function VendegLayout() {
     const { user } = useAuthContext(); 
-      return !user ? <>  <Navigacio /> {/* <NavJobbOldal/> */} <Outlet /> </>  : <Navigate to="/" />;
+      return !user ? <>  <NavigacioTeszt /> {/* <NavJobbOldal/> */} <Outlet /> </>  : <Navigate to="/" />;
 }
